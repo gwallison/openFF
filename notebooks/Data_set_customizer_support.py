a13416e35294db91ff4fc88553fce863e8bcd8c2
Gary Allison <gary.allison@gmail.com>
2023-10-04 15:18:25 -0400

    testing functionality of local/remote on notebooks

---
+++ notebooks/Data_set_customizer_support.py
@@ -0,0 +1,10 @@
+
+
+# import pandas as pd
+# from IPython.display import HTML, display
+# from IPython.display import Markdown as md
+# with open('welcome.md','r') as f:
+#     s = f.read()
+# display(md(s))
+
+

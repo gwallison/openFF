0b5a415bb5540b9d48b743b38e193a497ebb2f8d
Gary Allison <gary.allison@gmail.com>
2023-10-05 07:59:00 -0400

    moving shared routines to common/nb_helper

---
+++ common/nb_helper.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import requests
+import os
+
+def is_remote():
+    # check if we are not working on a known local machine
+    import platform
+    locals = ['Dell_2023_Gary']
+    if platform.node() in locals:
+        return False
+    return True
+
+def make_sandbox(name='sandbox'):
+    # make output location
+    tmp_dir = name
+    try:
+        os.mkdir(name)
+    except:
+        print(f'{name} already exists')
+        
+def get_size_of_url_file(url):
+    response = requests.head(url,allow_redirects=True)
+    return int(response.headers['Content-Length'])
+
+def fetch_file(url,fn):
+    # get file from url, save it at fn
+    sz = get_size_of_url(url)
+    if size>100000000:
+        print('Fetching file, please be patient...')
+    urllib.request.urlretrieve(url,fn)
+
+def get_df_from_file(df_url,df_fn,force_freshen=False,inp_format='parquet'):
+    # get file from url, checking first it it already exists, then convert to dataframe
+    if force_freshen:
+        fetch_file(df_url,df_fn);
+    else:
+        if os.path.isfile(df_fn):
+            print('File already downloaded')
+        else:    
+            fetch_file(df_url,df_fn);
+            
+    print('Creating full dataframe...')
+    assert inp_format=='parquet'
+    return pd.read_parquet(df_fn)
+
+def show_done():
+    print('done')
--- notebooks/Data_set_customizer_support.py
+++ notebooks/Data_set_customizer_support.py
@@ -4,31 +4,26 @@ import os
 import requests 
 import urllib
 
+# handles to use in notebook
 out_dir = 'sandbox'
-
-print('running Data_set_customizer_support')
-import openFF.common.nb_opener as nbo
-
-nbo.make_sandbox(out_dir)
-
-
 df_url = "https://storage.googleapis.com/open-ff-common/repos/current_repo/full_df.parquet"
 df_fn = os.path.join(out_dir,'full_df.parquet')
 
-response = requests.head(df_url,  # Example file 
-    allow_redirects=True
-)
-print(f"Full size on remote disk: {int(response.headers['Content-Length']):,} bytes") 
+from openFF.common.nb_helper import make_sandbox, get_df_from_file, show_done
 
-if os.path.isfile(df_fn):
-    print('Open-FF file already downloaded')
-else:    
-    print('Fetching it now, please be patient...')
-    urllib.request.urlretrieve(df_url,df_fn);
+make_sandbox(out_dir)
+df = get_df_from_file(df_url,df_fn)
+show_done()
 
-print('Creating full dataframe...')
-df = pd.read_parquet(df_fn)
-print(f'The full Open-FF data frame shape (rows,cols): {df.shape}')
+# if os.path.isfile(df_fn):
+#     print('Open-FF file already downloaded')
+# else:    
+#     print('Fetching it now, please be patient...')
+#     urllib.request.urlretrieve(df_url,df_fn);
+
+# print('Creating full dataframe...')
+# df = pd.read_parquet(df_fn)
+# print(f'The full Open-FF data frame shape (rows,cols): {df.shape}')
 
 # states
 import ipywidgets as widgets
@@ -43,6 +38,7 @@ def prep_states():
                     description='Select State(s)',
                     disabled=False
                 )
+    show_done()
     return states
 
 def show_inc_chem_checkbox():
@@ -52,4 +48,5 @@ def show_inc_chem_checkbox():
                         disabled=False,
                         indent=True
                     )
+    show_done()
     return include_chem

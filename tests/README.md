23f79256d5c3a44c814a3681f502bba1b42be9a2
Gary Allison <gary.allison@gmail.com>
2023-10-05 10:59:42 -0400

    first functional notebook

---
+++ tests/README.md
@@ -0,0 +1,5 @@
+# Unit Tests for openFF
+
+These tests are performed before the file set is committed to github.
+
+**Currently, these are setup to be performed on a local machine and will not work remotely**
--- tests/nb_helper_test.py
+++ tests/nb_helper_test.py
@@ -1,9 +1,27 @@
 # tests for nb_helper.py
 import sys
+import os
 sys.path.insert(0,'c:/MyDocs/integrated/openFF') # adjust to your setup
-from common.nb_helper import *
-def func(x):
-    return x + 1
 
-def test_answer():
-    assert func(3) == 4
+
+from common.nb_helper import *
+simp_url = "https://storage.googleapis.com/open-ff-common/test_fixtures/simple_df.parquet"
+simp_fn = os.path.join('test_fixture','simp_df.parquet')
+
+def test_file_size():
+    assert get_size_of_url_file(simp_url)==15053
+
+def test_mkdir():
+    assert os.path.exists('test_fixture')==False
+    make_sandbox('test_fixture')    
+    assert os.path.exists('test_fixture')==True
+    
+def test_fetch_df_from_file():
+    df = get_df_from_file(simp_url,simp_fn)
+    assert df.shape == (7,20)
+    
+def test_remove_fixture():
+    import shutil
+    shutil.rmtree('test_fixture')
+    assert os.path.exists('test_fixture')==False
+    

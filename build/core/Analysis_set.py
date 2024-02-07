324fe1ad4b0ef58de108931d92f2d90102daf20f
Gary Allison <gary.allison@gmail.com>
2024-02-07 15:41:00 -0500

    creating Flag Issues class

--- build/core/Analysis_set.py
+++ build/core/Analysis_set.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  5 14:22:20 2021
-
 @author: Gary
 
 """
-# import openFF.build.core.Table_manager as c_tab
 import pandas as pd
 import os
-from openFF.common.file_handlers import save_df, get_df
+import openFF.common.file_handlers as fh
 
 def make_full_set_file(sources,outdir):
     descfn = os.path.join(sources,'pickles','disclosures.parquet')
     recsfn = os.path.join(sources,'pickles','chemrecs.parquet')
     casfn = os.path.join(sources,'pickles','bgCAS.parquet')
+    wsfn =  os.path.join(sources,'pickles','water_source.parquet')
     outfn = os.path.join(outdir,'full_df.parquet')
     
-    df = pd.merge(get_df(descfn),get_df(recsfn),on='DisclosureId',how='inner')
-    df = pd.merge(df,get_df(casfn),on='bgCAS',how='left')
+    df = pd.merge(fh.get_df(descfn),get_df(recsfn),on='DisclosureId',how='inner')
+    df = pd.merge(fh.df,get_df(wsfn),on='DisclosureId',how='left',validate='m:1')
+    df = pd.merge(fh.df,get_df(casfn),on='bgCAS',how='left',validate='m:1')
     
     df['in_std_filtered'] = ~(df.is_duplicate)&~(df.dup_rec)
 
-    save_df(df,outfn)
+    fh.save_df(df,outfn)
     return df
 
 # def make_full_set_file_join(sources,outdir):
---
+++ build/core/flag_issues.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import os
+
+class Disclosure_Issues():
+    def __init__(self,df):
+        self.df = df
+        self.gb = df.groupby('DisclosureId',as_index=False)[['APINumber','TotalBaseWaterVolume']].first()
+
+    # ALL Issues must be named "dIssues_x"  where x is usually a consecutive number.
+    # x will become the flag's name as in "d_x"
+        
+    def get_disc_set(self,cond):
+        disc_set = self.gb[cond].DisclosureId.tolist()
+        return disc_set
+
+    def dIssue_1(self):
+        cond = ~(self.gb.TotalBaseWaterVolume>0)
+        return self.get_disc_set(cond)
+
+    def dIssue_2(self):
+        cond = self.gb.TotalBaseWaterVolume==10000
+        return self.get_disc_set(cond)
+
+
+class Record_Issues():
+    def __init__(self,df):
+        self.df = df
+
+    # ALL Issues must be named "rIssues_x"  where x is usually a consecutive number.
+    # x will become the flag's name as in "r_x"
+
+    def get_rec_set(self,cond):
+        reckey_set = self.df[cond].reckey.tolist()
+        return reckey_set
+
+    def rIssue_1(self):
+        c1 = self.df.dup_rec
+        return self.get_rec_set(c1)
+
+class Flag_issues():
+    """Used to detect known data issues in the FracFocus data and generate appropriate flags to warn users.
+    For each issue, a list is returned of DisclosureId (or IngredientsId) for the flags"""
+
+    def __init__(self,df):
+        self.df = df
+        self.dIssues = Disclosure_Issues(df)
+        self.rIssues = Record_Issues(df)
+        self.get_disc_issues()
+        self.get_rec_issues()
+
+    def get_disc_issues(self):
+        self.disc_issue_dic = {}
+        for item in dir(self.dIssues):
+            if item[:7] == 'dIssue_':
+                name = 'd_'+item[7:]
+                self.disc_issue_dic['self.dIssues.'+item+'()'] = name
+        # print(self.disc_issue_dic)
+
+    def make_disc_flag_df(self,disc_set,disc_issues):
+        self.disc_df = pd.DataFrame({'DisclosureId':list(disc_set)})
+        for issue in disc_issues:
+            self.disc_df[issue[1]] = self.disc_df.DisclosureId.isin(issue[0])
+
+
+    def get_rec_issues(self):
+        self.rec_issue_dic = {}
+        for item in dir(self.rIssues):
+            if item[:7] == 'rIssue_':
+                name = 'r_'+item[7:]
+                self.rec_issue_dic['self.rIssues.'+item+'()'] = name
+        print(self.rec_issue_dic)
+
+    def make_rec_flag_df(self,reckey_set,rec_issues):
+        self.rec_df = pd.DataFrame({'reckey':list(reckey_set)})
+        for issue in rec_issues:
+            self.rec_df[issue[1]] = self.rec_df.reckey.isin(issue[0])
+
+    def detect_all_issues(self):
+        disc_set = set()
+        names = []
+        disissues = []
+        for test in self.disc_issue_dic.keys():
+            name = self.disc_issue_dic[test]
+            lst = eval(test)
+            # print(lst)
+            print(f'generating flags for: {name}')
+            disc_set.update(lst)
+            names.append(name)
+            disissues.append((lst,name))
+        # make sure we don't have duplicate names
+        seen = set()
+        dupes = [x for x in names if x in seen or seen.add(x)]
+        assert len(dupes)== 0, f'Duplicates in flag names: {dupes}'
+        self.make_disc_flag_df(disc_set,disissues)
+
+        rec_set = set()
+        names = []
+        recissues = []
+        for test in self.rec_issue_dic.keys():
+            name = self.rec_issue_dic[test]
+            lst = eval(test)
+            print(f'generating flags for: {name}')            
+            rec_set.update(lst)
+            names.append(name)
+            recissues.append((lst,name))
+        # make sure we don't have duplicate names
+        seen = set()
+        dupes = [x for x in names if x in seen or seen.add(x)]
+        assert len(dupes)== 0, f'Duplicates in flag names: {dupes}'
+
+        self.make_rec_flag_df(rec_set,recissues)
+
+        # get the IDs into rec_df
+        # self.rec_df = pd.merge(self.df[['DisclosureId','IngredientsId','reckey']],
+        #                        self.rec_df,on='reckey',how='right',validate='1:1')
+
+    
